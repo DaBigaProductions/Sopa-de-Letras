586fdfa84faf9780ee7ea4201e820576fe0d4466
DaBigaProductions <ed13dress@gmail.com>
2025-05-27 17:32:57 -0500

    feat: creación archivo de base de datos

---
+++ sopadeletras.sql
@@ -0,0 +1,88 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-05-2025 a las 00:31:40
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sopadeletras`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `palabras`
+--
+
+CREATE TABLE `palabras` (
+  `id` int(11) NOT NULL,
+  `palabra` varchar(15) DEFAULT NULL,
+  `cuadricula` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `palabras`
+--
+
+INSERT INTO `palabras` (`id`, `palabra`, `cuadricula`) VALUES
+(1, 'AMOR', 7),
+(2, 'DATOS', 8),
+(3, 'SOFTWARE', 9),
+(4, 'GUITARRA', 10),
+(5, 'MUSICA', 11),
+(6, 'APLAUSO', 12),
+(7, 'FELICIDAD', 13),
+(8, 'VACACIONES', 14),
+(9, 'PASTEL', 15),
+(13, 'PALABRADIEZ', 16);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `records`
+--
+
+CREATE TABLE `records` (
+  `fecha` timestamp(6) NULL DEFAULT NULL,
+  `cuadricula` int(11) DEFAULT NULL,
+  `palabra` varchar(10) DEFAULT NULL,
+  `demora` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `palabras`
+--
+ALTER TABLE `palabras`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `palabras`
+--
+ALTER TABLE `palabras`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
